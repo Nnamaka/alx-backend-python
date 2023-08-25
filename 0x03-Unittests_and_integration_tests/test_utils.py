d733aa43797bbca7399f641b75f64c43907381ad
nnamaka <nnamaka7@gmail.com>
2023-08-25 12:33:18 +0100

    fixtures

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,15 +2,12 @@
 """A module for testing the utils module.
 """
 import unittest
-from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
+from typing import Dict, Tuple, Union
 from parameterized import parameterized
 
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+from utils import access_nested_map,get_json,memoize
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
